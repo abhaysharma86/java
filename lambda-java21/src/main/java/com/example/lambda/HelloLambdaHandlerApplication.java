373bcb44b11dfbc132752af719fd1ed4a731cf99
Abhay Sharma <sharmaabhay7248@gmail.com>
2026-03-05 08:06:03 +0530

    Inheritance topic

---
+++ lambda-java21/src/main/java/com/example/lambda/HelloLambdaHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLambdaHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloLambdaHandlerApplication.class, args);
+	}
+
+}
