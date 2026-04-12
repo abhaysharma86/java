3e71f4c93dd2d51f111bd9dd5b4dfc2e61424fef
Abhay Sharma <sharmaabhay7248@gmail.com>
2026-04-12 12:07:51 +0530

    stream

--- src/stream/Sum.java
+++ src/stream/Sum.java
@@ -1,17 +1,34 @@
 package stream;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Sum {
     public static void main(String[] args) {
-        List<Integer> list = List.of(8, 3, 5, 1, 9, 2);
+        List<Integer> list = List.of(0, 1, 2);
 
         int sum = list.stream().mapToInt(n -> n).sum();
         System.out.println(sum);
-        
-    }
 
+        Map<Integer, Long> collect = list.stream()
+                .filter(n -> n / 2 != 0)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(collect);
+
+        Set<Integer> collect1 = list.stream().filter(n -> Collections.frequency(list, n) > 1).collect(Collectors.toSet());
+        System.out.println(collect1);
+
+        Set<Integer> re = new HashSet<>();
+        for (int i : list) {
+            if (Collections.frequency(list, i) > 1) {
+                re.add(i);
+            }
+        }
+        System.out.println(re);
+
+    }
 
 
 }
